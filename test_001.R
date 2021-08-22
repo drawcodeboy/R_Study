579d3f13d2a08f8099cb68faea79a48f42fa746e
drawcodeboy <20rnjsekdns@naver.com>
2021-08-22 16:24:04 +0900

    오랜만에 커밋이다

---
+++ test_001.R
@@ -0,0 +1,16 @@
+#데이터 형(= 타입)
+#기본형 numeric, character, logical
+#구조형 scalar, factor, vector < matrix < array
+#복합형 list < data.frame < data.table
+#special values NULL, NA, NaN, INF
+rm(list=ls())
+ls()
+a <- 10
+b = 20
+hap <- a + b
+cat(a,'+', b, '=', hap, "\n")
+ls()
+rm(hap)
+ls()
+a_list = 20
+a.so = 10
